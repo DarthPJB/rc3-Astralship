e398f68590ef895895dbeba258736cf5e4ca0e1b
John Bargman <darthpjb@gmail.com>
2020-12-18 15:32:50 +0000

    Pushing initital tilesets and map test

---
+++ Default_template.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Default_template" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="tilesets/template7final.png" width="256" height="256"/>
+</tileset>
---
+++ cija_32x32_expansion for Pipoya_CC0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="cija_32x32_expansion for Pipoya_CC0" tilewidth="16" tileheight="16" tilecount="7980" columns="30">
+ <image source="tilesets/cija_32x32_expansion for Pipoya_CC0.png" width="480" height="4256"/>
+</tileset>
